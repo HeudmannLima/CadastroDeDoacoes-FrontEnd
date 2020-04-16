4c6ebd4315e353e885dba8a37b330ba5036bc61c
heudmann <heudmannlima@gmail.com>
2020-04-16 13:12:32 -0400

    Muda variavel ambiente da url API

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export default axios.create({
-  baseURL: process.env.REACT_APP_API_URL
-  // baseURL: 'https://cadastro-de-doacoes-backend.herokuapp.com/',
+  // baseURL: process.env.REACT_APP_API_URL
+  baseURL: 'https://cadastro-de-doacoes-backend.herokuapp.com/',
 });
